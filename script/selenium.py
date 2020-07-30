98218b9b08bb5419dd6471456807b4064b061159
naka345 <nakaka33@yahoo.co.jp>
2020-07-30 20:34:59 +0900

    classfied selenium selenium driver docker

--- script/selenium.py
+++ script/selenium.py
@@ -1,50 +1,89 @@
-import chromedriver_binary # nopa
+import os
+import chromedriver_binary
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
-# WebDriver のオプションを設定する
-options = webdriver.ChromeOptions()
-#options.add_argument('--headless')
+class QiitaAutoLogin(object):
+    # WebDriver のオプションを設定する
+    OPTIONS = webdriver.ChromeOptions()
+    OPTIONS.add_argument('--headless')
+    # r"https://127.0.0.1:5000/"
+    REDIRECT_URL = repr(os.environ.get("qiita_redirect_url"))
 
-print('connectiong to remote browser...')
-driver = webdriver.Chrome(options=options)
+    def __init__(self):
+        print('connectiong to remote browser...')
+        self.driver = webdriver.Chrome(options=OPTIONS)
 
-driver.get("https://qiita.com/login")
-elm = driver.find_element_by_class_name("btn-google-inverse")
-elm.click()
-print(elm)
+    def qiita_login_by_google_account(self):
+        login_url = "https://qiita.com/login"
+        self.driver.get(login_url)
+        google_auth_elm = self.driver.find_element_by_class_name("btn-google-inverse")
+        google_auth_elm.click()
 
-wait_time = 10
-login_id_xpath = '//*[@id="identifierNext"]'
-WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_id_xpath)))
-driver.find_element_by_tag_name("input").send_keys("nakaka33@gmail.com")
-driver.find_element_by_xpath(login_id_xpath).click()
+        google_login = LinkGoogleAccountsLogin()
+        google_login.auto_login(base_url)
 
-login_pw_xpath = '//*[@id="passwordNext"]'
-WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, login_pw_xpath)))
-WebDriverWait(driver, wait_time).until(EC.presence_of_element_located((By.XPATH, '//input[@name="password"]')))
-driver.find_element_by_xpath('//input[@name="password"]').send_keys("naka3465")
-driver.find_element_by_xpath(login_pw_xpath).click()
+class LinkGoogleAccountsLogin(QiitaAutoLogin):
+    EMAIL = os.environ.get("google_email")
+    PASSWORD = os.environ.get("google_password")
 
-WebDriverWait(driver, wait_time+10).until(EC.url_matches("https://qiita.com/"))
-print("qiita")
+    def __init__(self):
+        super().__init__()
 
-authorization_url, headers, body = oauth.prepare_authorization_request(
-    auth_url,
-    redirect_url=r"https://127.0.0.1:5000/",
-    scope = scope
-)
-driver.get(authorization_url)
-print(driver.current_url)
+    def auto_login(self, redirect_url):
+        self.email_input(wait_time=10)
+        self.password_input(wait_time=20)
+        # if completed input, called google two factor auth. Maybe, tap your smart phone
+        redirect_url = "https://qiita.com/"
+        self.google_login_wait(redirect_url, wait_time=30)
 
-accept_btn_xpath = '//input[@name="commit"]'
-WebDriverWait(driver, wait_time+20).until(EC.presence_of_element_located((By.XPATH, accept_btn_xpath)))
-driver.find_element_by_xpath(accept_btn_xpath).click()
+    def email_input(self, wait_time=10):
+        expected_btn_xpath = '//*[@id="identifierNext"]'
+        WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((By.XPATH, expected_btn_xpath)))
+        self.driver.find_element_by_tag_name("input").send_keys(self.EMAIL)
+        self.driver.find_element_by_xpath(expected_btn_xpath).click()
 
-print(driver.current_url)
-print(driver.title)
+    def password_input(self, wait_time=10):
+        expected_btn_xpath = '//*[@id="passwordNext"]'
+        input_pw_xpath = '//input[@name="password"]'
+        WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((By.XPATH, expected_btn_xpath)))
+        WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((By.XPATH, input_pw_xpath)))
+        self.driver.find_element_by_xpath(input_pw_xpath).send_keys(self.PASSWORD)
+        self.driver.find_element_by_xpath(expected_btn_xpath).click()
+
+    def google_login_wait(self, redirect_url, wait_time=10):
+        WebDriverWait(driver, wait_time).until(EC.url_matches(redirect_url))
+        print(f"redirected to: {self.driver.current_url}")
+
+class PassQiitaOAuth2(QiitaAutoLogin):
+
+    def __init__(self):
+        super().__init__()
+
+    def passed_oauth(self):
+        url = self.access_authorization()
+        self.accept_oauth(url)
+
+    def access_authorization(self):
+        authorization_url, headers, body = oauth.prepare_authorization_request(
+            auth_url,
+            redirect_url=self.REDIRECT_URL,
+            scope = scope
+        )
+        return authorization_url
+
+    def accept_oauth(self, authorization_url, wait_time=10):
+        self.driver.get(authorization_url)
+        print(self.driver.current_url)
+
+        accept_btn_xpath = '//input[@name="commit"]'
+        WebDriverWait(self.driver, wait_time).until(EC.presence_of_element_located((By.XPATH, accept_btn_xpath)))
+        self.driver.find_element_by_xpath(accept_btn_xpath).click()
+
+        print(self.driver.current_url)
+        print(self.driver.title)
 
 # ブラウザを終了する
 driver.quit()
